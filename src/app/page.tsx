b7e4c52341bbb94073ee522fd1cf66a320568477
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2026-06-12 21:16:20 +0300

    feat: default redirection to register page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
-
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      Script Buddy
-    </div>
-  );
+  redirect("/register");
 }
