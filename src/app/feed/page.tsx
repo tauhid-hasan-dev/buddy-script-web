30ae9a9f87b68584a43157a75bbcf656a520714a
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2026-06-12 12:43:28 +0300

    feat: parent component for feed

---
+++ src/app/feed/page.tsx
@@ -0,0 +1,77 @@
+import type { Metadata } from "next";
+
+import "@/styles/bootstrap.min.css";
+import "@/styles/common.css";
+import "@/styles/main.css";
+import "@/styles/responsive.css";
+
+import CreatePost from "@/components/feed/CreatePost";
+import FeedLayout from "@/components/feed/FeedLayout";
+import Header from "@/components/feed/Header";
+import LeftSidebar from "@/components/feed/LeftSidebar";
+import MobileBottomNav from "@/components/feed/MobileBottomNav";
+import MobileHeader from "@/components/feed/MobileHeader";
+import PostCard from "@/components/feed/PostCard";
+import RightSidebar from "@/components/feed/RightSidebar";
+import Stories from "@/components/feed/Stories";
+
+export const metadata: Metadata = {
+  title: "Buddy Script",
+};
+
+export default function FeedPage() {
+  return (
+    <>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500;600;700;800&display=swap"
+        rel="stylesheet"
+        precedence="default"
+      />
+      {/* Feed Section Start */}
+      <FeedLayout>
+        {/* Desktop Menu Start */}
+        <Header />
+        {/* Desktop Menu End */}
+        {/* Mobile Menu Start */}
+        <MobileHeader />
+        {/* Mobile Menu End */}
+        {/* Mobile Bottom Navigation */}
+        <MobileBottomNav />
+        {/* Mobile Bottom Navigation End */}
+        {/* Main Layout Structure */}
+        <div className="container _custom_container">
+          <div className="_layout_inner_wrap">
+            <div className="row">
+              {/* Left Sidebar */}
+              <div className="col-xl-3 col-lg-3 col-md-12 col-sm-12">
+                <LeftSidebar />
+              </div>
+              {/* Left Sidebar */}
+              {/* Layout Middle */}
+              <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+                <div className="_layout_middle_wrap">
+                  <div className="_layout_middle_inner">
+                    <Stories />
+                    <CreatePost />
+                    <PostCard />
+                    <PostCard />
+                  </div>
+                </div>
+              </div>
+              {/* Layout Middle */}
+              {/* Right Sidebar */}
+              <div className="col-xl-3 col-lg-3 col-md-12 col-sm-12">
+                <RightSidebar />
+              </div>
+              {/* Right Sidebar */}
+            </div>
+          </div>
+        </div>
+        {/* Main Layout Structure */}
+      </FeedLayout>
+      {/* Feed Section End */}
+    </>
+  );
+}
